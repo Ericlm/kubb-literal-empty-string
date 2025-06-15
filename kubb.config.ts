a70ce430c8141b07acfdb03f70ce7eb16e384235
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-06-15 12:40:31 +0200

    first commit

---
+++ kubb.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@kubb/core'
+
+export default defineConfig(() => {
+  return {
+    root: '.',
+
+    input: {
+      path: './petStore.yaml',
+    },
+
+    output: {
+      path: './src/gen',
+    },
+
+    plugins: [],
+  }
+})
